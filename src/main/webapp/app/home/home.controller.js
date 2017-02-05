ed98ed5c2711d0c85911de3d995ce733b104aca6
Topolev <i.topolev.vladimir@gmail.com>
2017-02-05 21:07:55 +0300

    Update index page: create navbar, main slider.

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -7,26 +7,54 @@
 
     HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
+    function HomeController($scope, Principal, LoginService, $state) {
         var vm = this;
 
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
         vm.register = register;
-        $scope.$on('authenticationSuccess', function() {
+
+        vm.currentIndexSlide = 0;
+        vm.slickConfig = {
+            autoplay: false,
+            autoplaySpeed: 1000,
+            event: {
+                beforeChange: function (event, slick, currentSlide, nextSlide) {
+                    console.log("Change")
+                },
+                afterChange: function (event, slick, currentSlide, nextSlide) {
+                }
+            }
+        }
+
+
+        vm.slides = [
+            {
+                "image": "content/images/main-carousel/001.jpg"
+            },
+            {
+                "image": "content/images/main-carousel/002.jpg",
+            },
+            {
+                "image" : "content/images/main-carousel/003.jpg",
+            }
+        ]
+        console.log(vm.slides.length)
+        $scope.$on('authenticationSuccess', function () {
             getAccount();
         });
 
         getAccount();
 
         function getAccount() {
-            Principal.identity().then(function(account) {
+            Principal.identity().then(function (account) {
                 vm.account = account;
                 vm.isAuthenticated = Principal.isAuthenticated;
             });
         }
-        function register () {
+
+        function register() {
             $state.go('register');
         }
     }
